8218cd6bb4f441cadacdf8b9136ccdc5b4d00454
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-01-19 16:00:17 -0500

    limpieza de librerias

--- application/views/layouts/links.php
+++ application/views/layouts/links.php
@@ -4,14 +4,8 @@
     <link href="<?php echo base_url();?>assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <!-- NProgress -->
     <link href="<?php echo base_url();?>assets/vendors/nprogress/nprogress.css" rel="stylesheet">
-    <!-- iCheck -->
-    <link href="<?php echo base_url();?>assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-
-    <!-- bootstrap-progressbar -->
-    <link href="<?php echo base_url();?>assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-
-    <!-- bootstrap-daterangepicker -->
-    <link href="<?php echo base_url();?>assets/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    
+    
 
     <!-- Custom Theme Style -->
     <link href="<?php echo base_url();?>assets/build/css/custom.min.css" rel="stylesheet">
--- application/views/layouts/scripts.php
+++ application/views/layouts/scripts.php
@@ -2,22 +2,10 @@
     <script src="<?php echo base_url();?>assets/vendors/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
     <script src="<?php echo base_url();?>assets/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url();?>assets/vendors/fastclick/lib/fastclick.js"></script>
+    
     <!-- NProgress -->
     <script src="<?php echo base_url();?>assets/vendors/nprogress/nprogress.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <!-- iCheck -->
-    <script src="<?php echo base_url();?>assets/vendors/iCheck/icheck.min.js"></script>
-    <!-- Skycons -->
-    <script src="<?php echo base_url();?>assets/vendors/skycons/skycons.js"></script>
-
-    <!-- DateJS -->
-    <script src="<?php echo base_url();?>assets/vendors/DateJS/build/date.js"></script>
-    <!-- bootstrap-daterangepicker -->
-    <script src="<?php echo base_url();?>assets/vendors/moment/min/moment.min.js"></script>
-    <script src="<?php echo base_url();?>assets/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+        
 
     <!-- Custom Theme Scripts -->
     <script src="<?php echo base_url();?>assets/build/js/custom.min.js"></script>
